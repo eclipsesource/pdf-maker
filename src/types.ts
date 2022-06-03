5c9f78fc8aa9ef6ec76fbde54e09c2dc2b374aaa
Ralf Sternberg <rsternberg@eclipsesource.com>
2022-06-03 10:34:29 +0200

    ♻️ Extract printValue function to dedicated module
    This function is used to format values for error messages. It's used in
    the `types` module, but its logic is completely independent.
    
    Extract it to a module of its own to clear up the types module.

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import { printValue } from './print-value.js';
+
 export type Obj = Record<string, unknown>;
 
 export type TypeDef<T> = (input: unknown) => T;
@@ -206,43 +208,4 @@ export function isObject(value: unknown): value is Record<string, unknown> {
 
 export function typeError(expected: string, value: unknown) {
   return new TypeError(`Expected ${expected}, got: ${printValue(value)}`);
-}
-
-export function printValue(value: unknown, refs?: unknown[]) {
-  if (typeof value === 'string') return `'${value}'`;
-  if (Array.isArray(value)) return printArray(value, refs);
-  if (value instanceof Date) return `Date ${value.toISOString()}`;
-  if (value instanceof Function) {
-    return value.name ? `function ${value.name}` : 'anonymous function';
-  }
-  if (value instanceof ArrayBuffer) return `ArrayBuffer ${printArray([...new Uint8Array(value)])}`;
-  if (ArrayBuffer.isView(value)) {
-    return `${value.constructor.name} ${printArray([...new Uint8Array(value.buffer)])}`;
-  }
-  const str = `${value}`;
-  if (str === '[object Object]') return printObject(value, refs);
-  return str;
-}
-
-function printArray(array: unknown[], refs?: unknown[]): string {
-  if (refs?.includes(array)) return 'recursive ref';
-  const maxElements = 8;
-  const content = array
-    .slice(0, maxElements)
-    .map((v) => printValue(v, [...(refs ?? []), array]))
-    .join(', ');
-  const tail = array.length > maxElements ? ', …' : '';
-  return `[${content}${tail}]`;
-}
-
-function printObject(object: unknown, refs?: unknown[]): string {
-  if (refs?.includes(object)) return 'recursive ref';
-  const maxEntries = 8;
-  const entries = Object.entries(object);
-  const tail = entries.length > maxEntries ? ', …' : '';
-  const main = entries
-    .slice(0, maxEntries)
-    .map(([key, value]) => `${key}: ${printValue(value, [...(refs ?? []), object])}`)
-    .join(', ');
-  return `{${main}${tail}}`;
 }
